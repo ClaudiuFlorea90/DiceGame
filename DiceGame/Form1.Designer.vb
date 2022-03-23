f4856d854723877a9fde89831b3159935a5c650f
Claudiu <Claudiu@DESKTOP-BSFLVVS>
2022-03-23 22:13:24 +0200

    Add project files.

---
+++ DiceGame/Form1.Designer.vb
@@ -0,0 +1,173 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Pic_Dice1 = New System.Windows.Forms.PictureBox()
+        Me.PicDice2 = New System.Windows.Forms.PictureBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.PrgProgressBar = New System.Windows.Forms.ProgressBar()
+        Me.TmrTimer = New System.Windows.Forms.Timer(Me.components)
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        CType(Me.Pic_Dice1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PicDice2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Pic_Dice1
+        '
+        Me.Pic_Dice1.Location = New System.Drawing.Point(12, 12)
+        Me.Pic_Dice1.Name = "Pic_Dice1"
+        Me.Pic_Dice1.Size = New System.Drawing.Size(150, 150)
+        Me.Pic_Dice1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.Pic_Dice1.TabIndex = 1
+        Me.Pic_Dice1.TabStop = False
+        '
+        'PicDice2
+        '
+        Me.PicDice2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.PicDice2.Location = New System.Drawing.Point(288, 12)
+        Me.PicDice2.Name = "PicDice2"
+        Me.PicDice2.Size = New System.Drawing.Size(150, 150)
+        Me.PicDice2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PicDice2.TabIndex = 1
+        Me.PicDice2.TabStop = False
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(367, 357)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(71, 29)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "Exit"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'PrgProgressBar
+        '
+        Me.PrgProgressBar.Location = New System.Drawing.Point(12, 357)
+        Me.PrgProgressBar.Name = "PrgProgressBar"
+        Me.PrgProgressBar.Size = New System.Drawing.Size(426, 29)
+        Me.PrgProgressBar.TabIndex = 4
+        Me.PrgProgressBar.Visible = False
+        '
+        'TmrTimer
+        '
+        '
+        'Button2
+        '
+        Me.Button2.BackgroundImage = CType(resources.GetObject("Button2.BackgroundImage"), System.Drawing.Image)
+        Me.Button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.Button2.Font = New System.Drawing.Font("MV Boli", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Button2.ForeColor = System.Drawing.SystemColors.InfoText
+        Me.Button2.Location = New System.Drawing.Point(168, 246)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(114, 105)
+        Me.Button2.TabIndex = 5
+        Me.Button2.Text = "Roll Dice!"
+        Me.Button2.TextAlign = System.Drawing.ContentAlignment.BottomCenter
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BackColor = System.Drawing.SystemColors.Control
+        Me.TextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TextBox1.Font = New System.Drawing.Font("Segoe UI Semibold", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.TextBox1.Location = New System.Drawing.Point(67, 168)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(34, 23)
+        Me.TextBox1.TabIndex = 6
+        Me.TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'TextBox2
+        '
+        Me.TextBox2.BackColor = System.Drawing.SystemColors.Control
+        Me.TextBox2.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TextBox2.Font = New System.Drawing.Font("Segoe UI Semibold", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.TextBox2.Location = New System.Drawing.Point(343, 168)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(32, 23)
+        Me.TextBox2.TabIndex = 6
+        Me.TextBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'TextBox3
+        '
+        Me.TextBox3.BackColor = System.Drawing.SystemColors.Control
+        Me.TextBox3.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TextBox3.Font = New System.Drawing.Font("Segoe UI Semibold", 25.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.TextBox3.Location = New System.Drawing.Point(168, 72)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(114, 58)
+        Me.TextBox3.TabIndex = 7
+        Me.TextBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'TextBox4
+        '
+        Me.TextBox4.BackColor = System.Drawing.SystemColors.Control
+        Me.TextBox4.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TextBox4.Font = New System.Drawing.Font("Comic Sans MS", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.TextBox4.Location = New System.Drawing.Point(12, 188)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(426, 28)
+        Me.TextBox4.TabIndex = 8
+        Me.TextBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(450, 396)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.PrgProgressBar)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.PicDice2)
+        Me.Controls.Add(Me.Pic_Dice1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Barbut"
+        CType(Me.Pic_Dice1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PicDice2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Pic_Dice1 As PictureBox
+    Friend WithEvents PicDice2 As PictureBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents PrgProgressBar As ProgressBar
+    Friend WithEvents TmrTimer As Timer
+    Friend WithEvents Button2 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+End Class
---
+++ DiceGame/Form1.vb
@@ -0,0 +1,133 @@
+﻿Public Class Form1
+
+    Dim Dice1, Dice2 As Integer
+    Dim Random As Random
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Application.Exit()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        TmrTimer.Start()
+        My.Computer.Audio.Play(My.Resources.dice_roll, AudioPlayMode.Background)
+    End Sub
+
+    Private Sub Lbl_DiceRoll_Click(sender As Object, e As EventArgs)
+
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged, TextBox2.TextChanged
+
+
+    End Sub
+
+    Private Sub TextBox3_TextChanged(sender As Object, e As EventArgs) Handles TextBox3.TextChanged
+
+    End Sub
+
+    Private Sub TextBox4_TextChanged(sender As Object, e As EventArgs) Handles TextBox4.TextChanged
+
+    End Sub
+
+    Private Sub PrgProgressBar_Click(sender As Object, e As EventArgs) Handles PrgProgressBar.Click
+
+    End Sub
+
+    Private Sub TmrTimer_Tick(sender As Object, e As EventArgs) Handles TmrTimer.Tick
+
+        Random = New Random
+        PrgProgressBar.Increment(5)
+
+        If PrgProgressBar.Value = 100 Then
+            TmrTimer.Stop()
+            PrgProgressBar.Value = 0
+            PrgProgressBar.Hide()
+            Button1.Show()
+            Button2.Show()
+            TextBox1.Hide()
+            TextBox2.Hide()
+            TextBox3.Show()
+            TextBox4.Show()
+        Else
+            PrgProgressBar.Show()
+            Button1.Hide()
+            Button2.Hide()
+            TextBox1.Show()
+            TextBox2.Show()
+            TextBox3.Hide()
+            TextBox4.Hide()
+            Dice1 = Random.Next(1, 7)
+            Dice2 = Random.Next(1, 7)
+            TextBox1.Text = Dice1
+            TextBox2.Text = Dice2
+            TextBox3.Text = Dice1 + Dice2
+
+            Dim num As Integer
+            num = Dice1 + Dice2
+
+            If num = 2 Then
+                TextBox4.Text = "2 BOSS? Du-te acasa ca esti slab"
+            ElseIf num = 3 Then
+                TextBox4.Text = "Pai ce facem aici sefule pierdem vremea?"
+            ElseIf num = 4 Then
+                TextBox4.Text = "Pfaaii the plm sa dus casa!"
+            ElseIf num = 5 Then
+                TextBox4.Text = "5? sa-mi bag pl am piedut masina!"
+            ElseIf num = 6 Then
+                TextBox4.Text = "Sa moara Veta daca nu zarurile sunt masluite"
+            ElseIf num = 7 Then
+                TextBox4.Text = "Ma omoara femeia acasa"
+            ElseIf num = 8 Then
+                TextBox4.Text = "Sa ma bata mama mai bine mergeam la pacanele"
+            ElseIf num = 9 Then
+                TextBox4.Text = "9? asta e tot ce aveam nevoie"
+            ElseIf num = 10 Then
+                TextBox4.Text = "Sa moara Gina, 10!"
+            ElseIf num = 11 Then
+                TextBox4.Text = "Ai de plm am pierdut banii de paine"
+            ElseIf num = 12 Then
+                TextBox4.Text = "Ai sa-mi da-i o tigara? manca-ti-as ochii!"
+            End If
+
+
+            If Dice1 = 1 Then
+                Pic_Dice1.Image = My.Resources.dice_1
+            ElseIf Dice1 = 2 Then
+                Pic_Dice1.Image = My.Resources.dice_2
+            ElseIf Dice1 = 3 Then
+                Pic_Dice1.Image = My.Resources.dice_3
+            ElseIf Dice1 = 4 Then
+                Pic_Dice1.Image = My.Resources.dice_4
+            ElseIf Dice1 = 5 Then
+                Pic_Dice1.Image = My.Resources.dice_5
+            ElseIf Dice1 = 6 Then
+                Pic_Dice1.Image = My.Resources.dice_6
+            End If
+
+            If Dice2 = 1 Then
+                PicDice2.Image = My.Resources.dice_1
+            ElseIf Dice2 = 2 Then
+                PicDice2.Image = My.Resources.dice_2
+            ElseIf Dice2 = 3 Then
+                PicDice2.Image = My.Resources.dice_3
+            ElseIf Dice2 = 4 Then
+                PicDice2.Image = My.Resources.dice_4
+            ElseIf Dice2 = 5 Then
+                PicDice2.Image = My.Resources.dice_5
+            ElseIf Dice2 = 6 Then
+                PicDice2.Image = My.Resources.dice_6
+            End If
+
+
+
+        End If
+
+    End Sub
+End Class
